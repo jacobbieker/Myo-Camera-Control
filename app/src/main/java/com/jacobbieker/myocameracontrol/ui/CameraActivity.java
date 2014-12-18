55d4380a59abfc5fc2275b8be675b058c72b8b5e
Jacob Bieker <jacob@jacobbieker.com>
2014-12-18 13:47:18 -0800

    zoom() added

--- app/src/main/java/com/jacobbieker/myocameracontrol/ui/CameraActivity.java
+++ app/src/main/java/com/jacobbieker/myocameracontrol/ui/CameraActivity.java
@@ -2,7 +2,6 @@ package com.jacobbieker.myocameracontrol.ui;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
