faf06b8bb95292e0bc82c2cff1227967502ae417
Jacob Bieker <jacob@jacobbieker.com>
2014-12-17 16:31:13 -0800

    Initial commit

---
+++ app/src/main/java/com/jacobbieker/myocameracontrol/CameraActivity.java
@@ -0,0 +1,246 @@
+package com.jacobbieker.myocameracontrol;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Toast;
+
+import com.commonsware.cwac.camera.*;
+import com.commonsware.cwac.camera.CameraFragment;
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.Quaternion;
+import com.thalmic.myo.XDirection;
+import com.thalmic.myo.scanner.ScanActivity;
+
+import java.io.IOException;
+
+
+public class CameraActivity extends Activity {
+
+    private static final String TAG = "Myo";
+    private static final String TAG_CAMERA_FRAGMENT = "camera_fragment";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        CameraFragment cameraFragment = new CameraFragment();
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, cameraFragment, TAG_CAMERA_FRAGMENT)
+                    .commit();
+        }
+
+        cameraFragment.setHost(new SimpleCameraHost(this));
+
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this, getPackageName())) {
+            // We can't do anything with the Myo device if the Hub can't be initialized, so exit.
+            Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        // Next, register for DeviceListener callbacks.
+        hub.addListener(mListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // We don't want any callbacks when the Activity is gone, so unregister the listener.
+        Hub.getInstance().removeListener(mListener);
+        if (isFinishing()) {
+            // The Activity is finishing, so shutdown the Hub. This will disconnect from the Myo.
+            Hub.getInstance().shutdown();
+        }
+    }
+
+    private void onScanActionSelected() {
+        // Launch the ScanActivity to scan for Myos to connect to.
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_camera, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (R.id.action_scan == id) {
+            onScanActionSelected();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Checks that the CameraFragment exists and is visible to the user,
+     * then takes a picture.
+     */
+    private void takePicture() {
+        CameraFragment f = (CameraFragment) getFragmentManager().findFragmentByTag(TAG_CAMERA_FRAGMENT);
+        if (f != null && f.isVisible()) {
+            f.takePicture();
+        }
+    }
+
+    /**
+     * Checks that the CameraFragment exists and is visible to the user,
+     * then focuses camera.
+     */
+    private void autoFocus() {
+        CameraFragment f = (CameraFragment) getFragmentManager().findFragmentByTag(TAG_CAMERA_FRAGMENT);
+        if (f != null && f.isVisible() && f.isAutoFocusAvailable()) {
+            f.autoFocus();
+        }
+    }
+
+    /**
+     * Checks that the CameraFragment exists and is visible to the user,
+     * then starts recording.
+     */
+    private void takeVideo() throws Exception {
+        CameraFragment f = (CameraFragment) getFragmentManager().findFragmentByTag(TAG_CAMERA_FRAGMENT);
+        if (f != null && f.isVisible()) {
+            f.record();
+        }
+    }
+
+    /**
+     * Checks that the CameraFragment exists and is visible to the user,
+     * then stops recording.
+     */
+    private void stopVideo() {
+        CameraFragment f = (CameraFragment) getFragmentManager().findFragmentByTag(TAG_CAMERA_FRAGMENT);
+        if (f != null && f.isVisible()) {
+            try {
+                f.stopRecording();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Myo Armband specific callbacks. Basis taken from MyoHelloWorld app on the Thalmic Developer Docs
+     */
+
+    // Classes that inherit from AbstractDeviceListener can be used to receive events from Myo devices.
+    // If you do not override an event, the default behavior is to do nothing.
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        private Arm mArm = Arm.UNKNOWN;
+        private XDirection mXDirection = XDirection.UNKNOWN;
+        // onConnect() is called whenever a Myo has been connected.
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to cyan when a Myo connects.
+        }
+        // onDisconnect() is called whenever a Myo has been disconnected.
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to red when a Myo disconnects.
+        }
+        // onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
+        // arm. This lets Myo know which arm it's on and which way it's facing.
+        @Override
+        public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+            mArm = arm;
+            mXDirection = xDirection;
+        }
+        // onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+        // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+        // when Myo is moved around on the arm.
+        @Override
+        public void onArmUnsync(Myo myo, long timestamp) {
+            mArm = Arm.UNKNOWN;
+            mXDirection = XDirection.UNKNOWN;
+        }
+        // onUnlock() is called whenever a synced Myo has been unlocked. Under the standard locking
+        // policy, that means poses will now be delivered to the listener.
+        @Override
+        public void onUnlock(Myo myo, long timestamp) {
+        }
+        // onLock() is called whenever a synced Myo has been locked. Under the standard locking
+        // policy, that means poses will no longer be delivered to the listener.
+        @Override
+        public void onLock(Myo myo, long timestamp) {
+        }
+        // onOrientationData() is called whenever a Myo provides its current orientation,
+        // represented as a quaternion.
+        @Override
+        public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
+            // Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
+            float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
+            float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
+            float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
+            // Adjust roll and pitch for the orientation of the Myo on the arm.
+            if (mXDirection == XDirection.TOWARD_ELBOW) {
+                roll *= -1;
+                pitch *= -1;
+            }
+            // Next, we apply a rotation to the text view using the roll, pitch, and yaw.
+        }
+        // onPose() is called whenever a Myo provides a new pose.
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            // Handle the cases of the Pose enumeration, and change the text of the text view
+            // based on the pose we receive.
+            switch (pose) {
+                case UNKNOWN:
+                    break;
+                case REST:
+                case DOUBLE_TAP:
+                    break;
+                case FIST:
+                    break;
+                case WAVE_IN:
+                    break;
+                case WAVE_OUT:
+                    break;
+                case FINGERS_SPREAD:
+                    break;
+            }
+            if (pose != Pose.UNKNOWN && pose != Pose.REST) {
+                // Tell the Myo to stay unlocked until told otherwise. We do that here so you can
+                // hold the poses without the Myo becoming locked.
+                myo.unlock(Myo.UnlockType.HOLD);
+                // Notify the Myo that the pose has resulted in an action, in this case changing
+                // the text on the screen. The Myo will vibrate.
+                myo.notifyUserAction();
+            } else {
+                // Tell the Myo to stay unlocked only for a short period. This allows the Myo to
+                // stay unlocked while poses are being performed, but lock after inactivity.
+                myo.unlock(Myo.UnlockType.TIMED);
+            }
+        }
+    };
+}
