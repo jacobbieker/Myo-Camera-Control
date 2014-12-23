e85a496c086cab28d8cab167e1c04f2c8129bae8
Jacob Bieker <jacob@jacobbieker.com>
2014-12-23 09:11:31 -0800

    Start creation of Help WebView

---
+++ app/src/main/java/com/jacobbieker/myocameracontrol/ui/HelpFragment.java
@@ -0,0 +1,9 @@
+package com.jacobbieker.myocameracontrol.ui;
+
+import android.webkit.WebViewFragment;
+
+/**
+ * Created by Jacob on 12/23/2014.
+ */
+public class HelpFragment extends WebViewFragment {
+}
