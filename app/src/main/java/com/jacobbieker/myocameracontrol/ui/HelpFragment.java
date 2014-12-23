28d6e13b238947757dc648da54ea380bd56f80f2
Jacob Bieker <jacob@jacobbieker.com>
2014-12-23 09:34:16 -0800

    Add loading index.html

--- app/src/main/java/com/jacobbieker/myocameracontrol/ui/HelpFragment.java
+++ app/src/main/java/com/jacobbieker/myocameracontrol/ui/HelpFragment.java
@@ -1,9 +1,35 @@
 package com.jacobbieker.myocameracontrol.ui;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.webkit.WebViewFragment;
+
+import com.jacobbieker.myocameracontrol.R;
 
 /**
  * Created by Jacob on 12/23/2014.
  */
 public class HelpFragment extends WebViewFragment {
+    private WebView webView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        Log.i("Fragment", "OnCreateView(): View Created");
+        webView = getWebView();
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        webView.loadUrl("index.html");
+    }
+
+
 }
